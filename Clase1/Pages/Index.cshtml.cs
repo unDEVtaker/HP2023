3601e2f5ff546c9e94378a4879defa6af1d931ec
Jonatan Silva <elundevtaker@gmail.com>
2023-10-27 21:34:59 -0300

    Initial commit

---
+++ Clase1/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clase1.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public List<string> Profesiones { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        var list = new List<string>();
+        
+        list.Add("vendedor");
+        list.Add("Analista");
+        list.Add("Administrativo");
+        list.Add("Profesor");
+        list.Add("Empresario");
+
+        Profesiones = list;
+        
+    }
+}
